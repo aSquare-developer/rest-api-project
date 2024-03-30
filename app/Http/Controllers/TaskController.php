ad40816e98ddcba86e531d127396a35bb1f48bd7
Artur-MacBook-Air <artur@example.com>
2024-03-31 00:09:55 +0200

    task #2

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,10 +7,15 @@ use Illuminate\Http\Request;
 use App\Models\Task;
 
 use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
     public function index(Request $request) {
       return new TaskCollection(Task::all());
+    }
+
+    public function show(Request $request, Task $task) {
+      return new TaskResource($task);
     }
 }
