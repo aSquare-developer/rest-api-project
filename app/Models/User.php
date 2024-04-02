a686fce3b8ad8d3ab4c08cb618cedcb0445aacc3
Artur-MacBook-Air <artur@example.com>
2024-04-02 21:31:24 +0300

    Auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,9 +10,11 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Task;
 
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
+
+Route::post('login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('tasks', TaskController::class);
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('tasks', TaskController::class);
+});
